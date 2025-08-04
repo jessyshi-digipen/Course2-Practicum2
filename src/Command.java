7d1806686cb5a9278c1e263be14c72cd9008465e
jessyshi-digipen <jessy.shi@digipen.edu>
2025-08-04 16:39:37 +0800

    Update Command.java
    updated email check

--- src/Command.java
+++ src/Command.java
@@ -9,9 +9,10 @@ public interface Command {
     //FIXME add in email format checks logic
     public default boolean checkEmail(String email){
 
-        String local = "^[a-zA-Z0-9]+([a-zA-Z0-9_.-]?[a-zA-Z0-9]+)+";
-        String domain = "@[a-zA-Z0-9]+([a-zA-Z0-9_.-]?[a-zA-Z0-9]+)+\\.[a-z]{2,3}$";
-        String regex = local + domain;
+        String local = "(^[a-zA-Z0-9]+([a-zA-Z0-9_.-]?[a-zA-Z0-9]+)+";
+        String domain = "@[a-zA-Z0-9]+([a-zA-Z0-9_.-]?[a-zA-Z0-9]+)+\\.[a-z]{2,3}$)";
+        String latin = " || (^\\w+$)";
+        String regex = local + domain +latin;
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
