1ba93b2bed80684345bddcd9573266bb75b0be71
yeeshaojie <62596282+yeeshaojie@users.noreply.github.com>
2025-08-05 16:55:52 +0800

    Update AddCommand.java
    add the inputChecker for addCommand()

--- src/Command/AddCommand.java
+++ src/Command/AddCommand.java
@@ -4,23 +4,13 @@ import Receiver.Receiver;
 public class AddCommand implements Command{
     Receiver receiver;
     String[] paramsStr;
-    String firstName, lastName, email;
 
     //constructor
     public AddCommand(Receiver receiver, String params) {
-        paramsStr = params.split(" ");
         //params contain 3 values (payload 1) <data1> <data2> <data3>
-        email = paramsStr[2];
-
-        //check the email format, do not assign receiver if the email format is wrong.
-        try {
-            if (!checkEmail(email)){
-                throw new IllegalArgumentException("Invalid email. Please enter valid email address");
-            }
+        while (inputChecker(params)){
+            this.paramsStr = params.split(" ");
             this.receiver = receiver;
-        }
-        catch (IllegalArgumentException iae) {
-            System.out.println("Invalid email");
         }
     }
 
