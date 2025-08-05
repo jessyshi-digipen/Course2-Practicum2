a32c00421e73ac5eaaba001301c6bdc7b263c3fe
jessyshi-digipen <jessy.shi@digipen.edu>
2025-08-05 16:36:39 +0800

    Update Invoker.java

--- src/Invoker/Invoker.java
+++ src/Invoker/Invoker.java
@@ -3,40 +3,52 @@ package Invoker;
 import Command.Command;
 import Command.UndoCommand;
 import Command.ListCommand;
+import CustomException.CustomException;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class Invoker {
+
     Command[] cmdToExecute;
 
-    //takes an array of Command, and push each command to the Stack CommandStackHistory
+
+
+    //takes an array of Command.Command, and push each command to the Stack CommandStackHistory
     public void setCommandsForExecution(Command[] cmdToExecute) {
         this.cmdToExecute = cmdToExecute;
+
     }
 
     //execute all the command in the stack
     public void executeCommand(Stack<Command> history) {
         for (Command cmd : cmdToExecute) {
             //Undo last undoable command if exist (update, add, delete)
-            try {
+
+            try{
                 if (cmd.getClass() == UndoCommand.class) {
-                while (!history.isEmpty() && history.peek().getClass() == ListCommand.class) {
-                    history.pop();
+                    while (!history.isEmpty() && history.peek().getClass() == ListCommand.class) {
+                        history.pop();
+                    }
+                    if (!history.isEmpty()) {
+                        Command lastCmd = history.pop();
+                        lastCmd.undo();
+                    }
                 }
-                if (!history.isEmpty()) {
-                    Command lastCmd = history.pop();
-                    lastCmd.undo();
+                else{
+                    cmd.execute();
+//
+                    if (cmd.getClass() != ListCommand.class) {
+                        history.push(cmd);
+                    }
                 }
             }
-            else{
-                cmd.execute();
-                if (cmd.getClass() != ListCommand.class) {
-                    history.push(cmd);
-                }
-            } catch (CustomException e) {
+            catch (CustomException e){
                 System.out.println(e.getmessage());
             }
+
+
+
+
         }
     }
 }
