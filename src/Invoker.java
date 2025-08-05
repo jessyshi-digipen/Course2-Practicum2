c792adcbdff35bb18049b399e4a4ac3e4a26aa54
yeeshaojie <62596282+yeeshaojie@users.noreply.github.com>
2025-08-05 16:17:41 +0800

    Update Invoker.java
    add a try catch for executing the command in executeCommand()

--- src/Invoker.java
+++ src/Invoker.java
@@ -2,22 +2,19 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class Invoker {
-
     Command[] cmdToExecute;
-
-
 
     //takes an array of Command, and push each command to the Stack CommandStackHistory
     public void setCommandsForExecution(Command[] cmdToExecute) {
         this.cmdToExecute = cmdToExecute;
-
     }
 
     //execute all the command in the stack
     public void executeCommand(Stack<Command> history) {
         for (Command cmd : cmdToExecute) {
             //Undo last undoable command if exist (update, add, delete)
-            if (cmd.getClass() == UndoCommand.class) {
+            try {
+                if (cmd.getClass() == UndoCommand.class) {
                 while (!history.isEmpty() && history.peek().getClass() == ListCommand.class) {
                     history.pop();
                 }
@@ -28,14 +25,12 @@ public class Invoker {
             }
             else{
                 cmd.execute();
-//
                 if (cmd.getClass() != ListCommand.class) {
                     history.push(cmd);
                 }
+            } catch (CustomException e) {
+                System.out.println(e.getmessage());
             }
-
-
-
         }
     }
 }
