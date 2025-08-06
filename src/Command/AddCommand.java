f02eea32650f8b1357cde81889575f60dc160657
yeeshaojie <62596282+yeeshaojie@users.noreply.github.com>
2025-08-06 20:57:22 +0800

    Refactor AddCommand error handling

--- src/Command/AddCommand.java
+++ src/Command/AddCommand.java
@@ -4,39 +4,27 @@ import Receiver.Receiver;
 
 public class AddCommand implements Command{
     private Receiver receiver;
+    private String params;
     private String[] paramsStr;
     private String email;
 
     //constructor
     public AddCommand(Receiver receiver, String params) {
-        //split the params string and ensure correct number of inputs, otherwise, throw exception
-        try {
-            paramsStr = params.split(" ");
-            if(paramsStr.length != 3) {
-                throw new CustomException("Please enter correct number of parameters for AddCommand");
-            }
-        }
-        catch(CustomException ce) {
-            System.out.println(ce.getMessage());
-        }
-
-        //params contain 3 values (payload 1) <data1> <data2> <data3>
-        email = paramsStr[2];
-
-        //check the email format, do not assign receiver if the email format is wrong.
-        try {
-            if (!checkEmail(email)){
-                throw new CustomException("Invalid email. Please enter valid email address");
-            }
-            this.receiver = receiver;
-        }
-        catch (CustomException ce) {
-            System.out.println(ce.getMessage());
-        }
+        this.receiver = receiver;
+        this.params =  params;
     }
 
     @Override
-    public void execute() {
+    public void execute() throws CustomException {
+        //split the params string and ensure correct number of inputs, otherwise, throw exception
+        this.paramsStr = this.params.split(" ");
+        if(paramsStr.length != 3) {
+            throw new CustomException("Please enter correct number of parameters for AddCommand");
+        }
+        //check the email format, do not assign receiver if the email format is wrong.
+        if (!checkEmail(paramsStr[2])){
+            throw new CustomException("Invalid email. Please enter valid email address");
+        }
         //capitalize first 2 elements for first name and lastname
         for(int j=0;j<2;j++){
             paramsStr[j]= paramsStr[j].substring(0,1).toUpperCase() + paramsStr[j].substring(1).toLowerCase();
