ad1c4c9be6032d14fef3e38a511f625e218234b2
yeeshaojie <62596282+yeeshaojie@users.noreply.github.com>
2025-08-06 23:48:50 +0800

    Update Command: update regex

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -11,6 +11,10 @@ public interface Command {
 
     public default boolean checkEmail(String email){
 
+        //FIXME current regex does not accept a@aa.com or aa@a.com as email and accpets aa@a.a.com as email
+        // updated solution: ^([a-zA-Z0-9_]+(([.-]?[a-zA-Z0-9_])?)+?@[a-zA-Z0-9]+(([.-]?[a-zA-Z0-9])?)+?(\.[a-z]{2,3})+|\w+)$
+        // truncate: ^([\w]+(([.-]?[\w])?)+?@[a-zA-Z\d]+(([-]?[a-zA-Z\d])?)+?(\.[a-z]{2,3})+|\w+)$
+
         String local = "^([a-zA-Z0-9_]+([.-]?[a-zA-Z0-9_]+)+";
         String domain = "@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)+(\\.[a-z]{2,3})+";
         String latin = "|\\w+)$";
