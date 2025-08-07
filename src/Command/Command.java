5597d776535efc31e8acdf80ad8d188602157fa4
yeeshaojie <62596282+yeeshaojie@users.noreply.github.com>
2025-08-07 13:36:51 +0800

    Update Command: changed regex

--- src/Command/Command.java
+++ src/Command/Command.java
@@ -1,29 +1,28 @@
 package Command;
 
+import CustomException.CustomException;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public interface Command {
-    void execute();
-    void undo();
+    void execute() throws CustomException;
+    void undo() throws CustomException;
     boolean checkUndo();
 
 
     public default boolean checkEmail(String email){
 
-        //FIXME current regex does not accept a@aa.com or aa@a.com as email and accpets aa@a.a.com as email
-        // updated solution: ^([a-zA-Z0-9_]+(([.-]?[a-zA-Z0-9_])?)+?@[a-zA-Z0-9]+(([.-]?[a-zA-Z0-9])?)+?(\.[a-z]{2,3})+|\w+)$
-        // truncate: ^([\w]+(([.-]?[\w])?)+?@[a-zA-Z\d]+(([-]?[a-zA-Z\d])?)+?(\.[a-z]{2,3})+|\w+)$
+//        String local = "^([a-zA-Z0-9_]+([.-]?[a-zA-Z0-9_]+)+";
+//        String domain = "@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)+(\\.[a-z]{2,3})+";
+//        String latin = "|\\w+)$";
+//        String regex = local + domain +latin;
 
-        String local = "^([a-zA-Z0-9_]+([.-]?[a-zA-Z0-9_]+)+";
-        String domain = "@[a-zA-Z0-9]+([.-]?[a-zA-Z0-9]+)+(\\.[a-z]{2,3})+";
-        String latin = "|\\w+)$";
-        String regex = local + domain +latin;
+        String regex = "^([\\w]+(([.-]?[\\w])?)+?@[a-zA-Z\\d]+(([.-]?[a-zA-Z\\d])?)+?(\\.[a-z]{2,3})+|\\w+)$";
 
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(email);
 
         return matcher.find();
-
     }
 }
